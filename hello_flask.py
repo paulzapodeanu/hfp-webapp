63a9610c4478320bd15d473b5b0cc06fe9025b14
paulzapodeanu <paul.zapodeanu@gmail.com>
2020-03-10 21:24:27 +0200

    more html and getting the app ready to run in python anywhere

--- hello_flask.py
+++ hello_flask.py
@@ -1,22 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('life, the universe, and everything', 'eiru,!'))
-
-@app.route('/entry')
+    phrase=request.form['phrase']
+    letters=request.form['letters']
+    title='Here are your results:'
+    results=str(search4letters(phrase,letters))
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results)
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
-
-
-app.run(debug=1)
+if __name__ == '__main__':
+    app.run(debug=True)
