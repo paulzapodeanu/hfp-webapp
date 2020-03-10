e8ac67a5488ee12c191c8f9a0a4d3efed40ee7bf
paulzapodeanu <paul.zapodeanu@gmail.com>
2020-03-10 20:02:20 +0200

    initial commit

--- vsearch.py
+++ vsearch.py
@@ -1,7 +1,7 @@
 def search4vowels(phrase:str) -> set:
     """Return any vowels foun in a supplied phrase."""
     vowels = set('aeiou')
-    return vowels.intesection(set(phrase))
+    return vowels.intersection(set(phrase))
 def search4letters(phrase:str, letters:str='aeiou') -> set:
     """Return a set of the 'letters' found in 'phrase'."""
-    return set(letters).intesection(set(phrase))
+    return set(letters).intersection(set(phrase))
